bf90856b4aecb5b205df62b19e7bb8448eadce4b
senniladri35 <senniladri35@gmail.com>
2024-04-06 20:38:17 +0530

    Adding Spinner

--- src/Body.js
+++ src/Body.js
@@ -1,5 +1,5 @@
-import { useEffect,useState } from 'react';
-import {Button, Container, Form,Row,Col} from 'react-bootstrap';
+import { useState } from 'react';
+import {Button, Container, Form,Row,Col, Spinner} from 'react-bootstrap';
 import { fetchData } from './utils/fetchData';
 import JobCard from './JobCard';
 
@@ -7,11 +7,16 @@ export default function Body(){
 
     const [searchJob, setSearchJob] = useState('');
     const [jobData, setJobData] = useState([]);
+    const [loader, setLoader] = useState(false);
     
     async function handleClick(){
-        const jobData = await fetchData(searchJob);
+        setLoader(true);
+        await fetchData(searchJob)
+        .then((jobData)=> {setLoader(false);
          setJobData(jobData.data);
         console.log(jobData.data);
+        });
+        
     }
 
     return (
@@ -27,6 +32,7 @@ export default function Body(){
         </Form>
         
         <Container className='mt-5'>
+        {loader ? <Spinner size="xls" style={{ position: "fixed", top: "50%", left: "50%", transform: "translate(-50%, -50%)" }} animation="grow"/> :
         <Row xs={1} md={3} className="g-4 mt-3">
             {jobData && jobData.map((job,index)=>{
                 return ( 
@@ -39,11 +45,11 @@ export default function Body(){
 
             })}
             </Row>
-
+        }
         </Container>
         </div>
 
-
+       
         </>
     )
       
